76b1ebd9f923681ed52a3f7d2bc03b67b0f81834
Thomas Mailund <mailund@birc.au.dk>
2024-01-29 12:27:55 +0100

    Updates

--- hash.c
+++ hash.c
@@ -1,71 +1,62 @@
 
 #include "hash.h"
+
 #include <stdlib.h>
 #include <string.h>
 
-struct hash_table *empty_table(size_t size)
-{
-    // Allocate table and bins
-    struct hash_table *table = malloc(sizeof *table);
-    *table = (struct hash_table){
-        .bins = malloc(size * sizeof *table->bins),
-        .size = size
-    };
+struct hash_table *empty_table(size_t size) {
+  // Allocate table and bins
+  struct hash_table *table = malloc(sizeof *table);
+  table->size = size;
+  table->bins = malloc(size * sizeof *table->bins);
 
-    // Set all bins to free
-    for (struct bin *bin = table->bins; bin != table->bins + size; bin++) {
-        bin->is_free = true;
-    }
+  // Set all bins to free
+  for (struct bin *bin = table->bins; bin != table->bins + size; bin++) {
+    bin->is_free = true;
+  }
 
-    return table;
+  return table;
 }
 
-void delete_table(struct hash_table *table)
-{
-    free(table->bins);
-    free(table);
+void delete_table(struct hash_table *table) {
+  free(table->bins);
+  free(table);
 }
 
-static inline 
-unsigned int hash_bin_index(struct hash_table *table, hash_key key)
-{
-    // Using bit masking to get the bin index.
-    unsigned int mask = table->size - 1;
-    unsigned int index = key & mask; 
-    // Alternatively, we can use key % table->size;
-    return index;
+static inline unsigned int hash_bin_index(struct hash_table *table,
+                                          hash_key key) {
+  // Using bit masking to get the bin index.
+  // Alternatively, we can use key % table->size;
+  unsigned int mask = table->size - 1;
+  unsigned int index = key & mask;
+  return index;
 }
 
-static inline 
-struct bin *hash_bin(struct hash_table *table, hash_key key)
-{
-    return table->bins + hash_bin_index(table, key);
+static inline struct bin *hash_bin(struct hash_table *table, hash_key key) {
+  return table->bins + hash_bin_index(table, key);
 }
 
-void insert_key(struct hash_table *table, hash_key key)
-{
-    struct bin *bin = hash_bin(table, key);
-    if (bin->is_free) {
-        bin->key = key;
-        bin->is_free = false;
-    } else {
-        // There is already a key here, so we have a
-        // collision. We cannot deal with this yet.
-    }
+void insert_key(struct hash_table *table, hash_key key) {
+  struct bin *bin = hash_bin(table, key);
+  if (bin->is_free) {
+    bin->key = key;
+    bin->is_free = false;
+  } else {
+    // There is already a key here, so we have a
+    // collision. We cannot deal with this yet.
+  }
 }
 
-bool contains_key(struct hash_table *table, hash_key key)
-{
-    struct bin *bin = hash_bin(table, key);
-    // The bin contains the key if it isn't empty and the key it contains is the
-    // one we are looking for.
-    return !bin->is_free && bin->key == key;
+bool contains_key(struct hash_table *table, hash_key key) {
+  struct bin *bin = hash_bin(table, key);
+  // The bin contains the key if it isn't empty and the key
+  // it contains is the one we are looking for.
+  return !bin->is_free && (bin->key == key);
 }
 
-void delete_key(struct hash_table *table, hash_key key)
-{
-    struct bin *bin = hash_bin(table, key);
-    if (bin->key == key) {
-        bin->is_free = true;
-    }
+void delete_key(struct hash_table *table, hash_key key) {
+  // Set the bin to free if the key matches, otherwise not (it contains a
+  // different key)
+  struct bin *bin = hash_bin(table, key);
+  bin->is_free = (bin->key == key);
 }
--- hash_flex.c
+++ hash_flex.c
@@ -1,39 +1,40 @@
 
-#include "hash.h"
 #include <stdlib.h>
 #include <string.h>
+
+#include "hash.h"
 
 struct hash_table *empty_table(size_t size)
 {
     // Allocate table and bins
     struct hash_table *table = malloc(sizeof *table + size * sizeof *table->bins);
+    if (!table)
+        return table;
+
     table->size = size;
 
     // Set all bins to free
-    for (struct bin *bin = table->bins; bin != table->bins + size; bin++) {
+    for (struct bin *bin = table->bins; bin != table->bins + size; bin++)
+    {
         bin->is_free = true;
     }
 
     return table;
 }
 
-void delete_table(struct hash_table *table)
-{
-    free(table->bins);
-}
+void delete_table(struct hash_table *table) { free(table->bins); }
 
-static inline 
-unsigned int hash_bin_index(struct hash_table *table, hash_key key)
+static inline unsigned int hash_bin_index(struct hash_table *table,
+                                          hash_key key)
 {
     // Using bit masking to get the bin index.
     unsigned int mask = table->size - 1;
-    unsigned int index = key & mask; 
+    unsigned int index = key & mask;
     // Alternatively, we can use key % table->size;
     return index;
 }
 
-static inline 
-struct bin *hash_bin(struct hash_table *table, hash_key key)
+static inline struct bin *hash_bin(struct hash_table *table, hash_key key)
 {
     return table->bins + hash_bin_index(table, key);
 }
@@ -41,10 +42,13 @@ struct bin *hash_bin(struct hash_table *table, hash_key key)
 void insert_key(struct hash_table *table, hash_key key)
 {
     struct bin *bin = hash_bin(table, key);
-    if (bin->is_free) {
+    if (bin->is_free)
+    {
         bin->key = key;
         bin->is_free = false;
-    } else {
+    }
+    else
+    {
         // There is already a key here, so we have a
         // collision. We cannot deal with this yet.
     }
@@ -60,8 +64,7 @@ bool contains_key(struct hash_table *table, hash_key key)
 
 void delete_key(struct hash_table *table, hash_key key)
 {
+    // Set the bin to free if the key matches, otherwise not (it contains a different key)
     struct bin *bin = hash_bin(table, key);
-    if (bin->key == key) {
-        bin->is_free = true;
-    }
+    bin->is_free = (bin->key == key);
 }
